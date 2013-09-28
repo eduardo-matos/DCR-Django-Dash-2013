ff8293b5cd2da2dd62993e90f2bba3e3a63c0c1b
Eduardo Matos <edu.de.matos@hotmail.com>
2013-09-28 13:58:13 -0300

    first implementation of recursive validator

--- dcr/core/views.py
+++ dcr/core/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from py_w3c.validators.html.validator import HTMLValidator
+from recursive_validator.validator import Validator 
 
 def home(request):
     return render_to_response("core/index.html")
---
+++ recursive_validator/validator.py
@@ -0,0 +1,40 @@
+# coding utf-8
+
+from py_w3c.validators.html.validator import HTMLValidator
+from pyquery import PyQuery as pq
+from urlparse import urlparse
+
+
+class Validator(object):
+    url = None
+    errors = None
+    warnings = None
+    anchors=[]
+    errors= {}
+    threshold = 3
+
+    _validator_w3c = None
+
+    def __init__(self, url):
+        self.url = url
+        self._validator_w3c = HTMLValidator()
+
+        self.anchors = self._fetch_anchors()
+        self.anchors.append(self.url)
+
+        a = [anchor for anchor in self.anchors if urlparse(anchor).netloc and urlparse(anchor).netloc in self.url]
+
+        self.anchors = list(set(a))
+
+        for an in self.anchors:
+            self._validator_w3c.validate(an)
+            self.errors[an] = self._validator_w3c.errors
+
+    def _fetch_anchors(self):
+        d = pq(url=self.url)
+
+        anchors = []
+        for anchor in d('a[href]'):
+            anchors.append(anchor.attrib['href'])
+
+        return anchors
