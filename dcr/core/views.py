fc52b476108cdaf9904fb6023e8d524947960479
Eduardo Matos <edu.de.matos@hotmail.com>
2013-09-27 23:15:35 -0300

    add py_w3c package
    since heroku is not pulling module from requirements,
    I added it manually

--- dcr/core/views.py
+++ dcr/core/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
 
 def home(request):
-	return HttpResponse('Hello world!')
+    from py_w3c.validators.html.validator import HTMLValidator
+
+    vld = HTMLValidator()
+
+    # validate
+    vld.validate("http://www.comprafacil.com.br/")
+
+    # look for warnings
+    return HttpResponse(vld.errors[0]['message'])
